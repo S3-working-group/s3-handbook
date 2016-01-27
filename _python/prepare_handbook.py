28d65490f9d8257b74b8508e22385bbd2eb300d2
bboc <bernhard.bockelbrink@gmail.com>
2016-01-27 22:03:21 +0100

    handbook compiles to one mmd file (but no headlines)

--- _python/prepare_handbook.py
+++ _python/prepare_handbook.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import shutil
 
 from s3_patterns import s3_patterns, handbook_group_order, all_patterns
 from common import make_pathname, make_title, create_directory
@@ -15,21 +16,31 @@ def prepare_handbook(args):
     if set(handbook_group_order) != set(s3_patterns.keys()):
         raise Exception("ERROR: Handbook group order does not reflect actual pattern groups!")
 
-    # copy_all_files_to_tmp(dst_dir, patterns)
+    copy_all_files_to_tmp(dst_dir, patterns)
 	# 2. remove front matter, add titles with correct level to patterns and groups
-	# 3. build the master file with transclude files for all patterns and groups
-	# 4. transclude and build happens in build-handbook.sh
-
 	
+    create_master_file_with_all_patterns(dst_dir)
+	
+
 def copy_all_files_to_tmp(dst_dir, patterns):
     """Copy all pattern and group--content files to handbook/tmp."""
     for pattern in patterns:
         shutil.copy('%s.md' % make_pathname(pattern), dst_dir)
 
     for group in sorted(s3_patterns.keys()):
-        shutil.copy('%s.md' % make_pathname(group), dst_dir)        
+        shutil.copy('%s--content.md' % make_pathname(group), dst_dir)        
 
     shutil.copy('changelog.md', dst_dir)        
+
+
+def create_master_file_with_all_patterns(dst_dir):
+    """Create tmp/patterns--master.md with transcludes for all groups and their patterns."""
+    with file(os.path.join(dst_dir, 'patterns--master.md'), 'w+') as fp:
+        for group in handbook_group_order:
+            fp.write('\n\n{{%s--content.md}}\n' % make_pathname(group))
+            for pattern in sorted(s3_patterns[group]):
+                fp.write('\n{{%s.md}}\n' % make_pathname(pattern))
+
 
 
 if __name__ == "__main__":
--- build-handbook.sh
+++ build-handbook.sh
@@ -1,4 +1,5 @@
 # prepare handbook build
-# python _python/prepare_handbook.py
+python _python/prepare_handbook.py
+
 # transclude all to one file 
-# multimarkdown --to=mmd --output=handbook/handbook-full.md handbook/handbook--master.md
+multimarkdown --to=mmd --output=handbook/handbook-full.md handbook/handbook--master.md
